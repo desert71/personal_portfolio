8593f57a99468404a2293962428735b216a3cddd
Desert71 <amkazennov99@gmail.com>
2022-09-01 11:59:14 +0300

    Creating blogs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class Blogs(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    text = models.CharField(max_length=50000)
+
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views as port_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', port_views.home)
+    path('', port_views.home),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
